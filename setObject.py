fac5647a850129e24cc89f75dad2da6c3a6e6069
koki <144724757+helpkoki@users.noreply.github.com>
2024-11-24 22:17:17 +0200

    futura: wrap-file

---
+++ setObject.py
@@ -0,0 +1,28 @@
+import hashlib
+import os
+
+class setObject(object):
+      
+      def  __init__(self, file ):
+           self.worktree = os.getcwd()
+           self.gitdir = os.path.join(self.worktree, ".git")
+           self.tohash =  os.path.join(self.gitdir ,file)
+              # Ensure we're in a Git repository
+           if not ( os.path.isdir(self.gitdir)):
+              raise Exception(f"Not a Git repository: {self.worktree}")
+               # Construct the object path
+           if not( os.path.isfile(self.gitdir(self.tohash))):
+              raise Exception(f"could not open: {self.worktree} for reading: No such file or directory")
+
+      def wrap_file(file_path):
+        with open(file_path, 'r') as f:
+          content = f.read()
+          size = len(content)  # Calculate size in bytes
+          wrapped = f"blob {size}\0{content}"
+          return wrapped
+          
+
+       
+            
+     
+       
